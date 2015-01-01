f433b6f2baa3e295bf1ba994a780a171a98e7358
Flash <HeyFlash@gmx.net>
2015-01-01 20:50:10 +0100

    changed readme options

--- src/SFML/Audio/ClaspBindingAudio.cpp
+++ src/SFML/Audio/ClaspBindingAudio.cpp
@@ -1,3 +1,4 @@
+#include <SFML/Audio/Listener.hpp>
 #include <SFML/Audio/Music.hpp>
 #include <SFML/Audio/Sound.hpp>
 #include <SFML/Audio/SoundBuffer.hpp>
@@ -27,8 +28,145 @@ void CLASP_MAIN()
     using namespace clbind;
     package("SF")
 	[
-	 //Question to drmeister: What do we do with static functions?
 	 //class_<sf::Listener>("listener")
+	 // def("listener/set-global-volume", &sf::Listener::setGlobalVolume,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Change the global volume of all the sounds and musics
+
+	 //      The volume is a number between 0 and 100; it is combined with
+	 //      the individual volume of each sound / music.
+	 //      The default value for the volume is 100 (maximum).
+
+	 //      \param volume New global volume, in the range [0, 100]
+
+	 //      \see getGlobalVolume**)")
+
+	 // ,def("listener/get-global-volume", &sf::Listener::getGlobalVolume,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Get the current value of the global volume
+
+	 //      \return Current global volume, in the range [0, 100]
+
+	 //      \see setGlobalVolume**)")
+
+	 // ,def("listener/set-position", &sf::Listener::setPosition,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Set the position of the listener in the scene
+
+	 //      The default listener's position is (0, 0, 0).
+
+	 //      \param x X coordinate of the listener's position
+	 //      \param y Y coordinate of the listener's position
+	 //      \param z Z coordinate of the listener's position
+
+	 //      \see getPosition, setDirection**)")
+
+	 // ,def("listener/set-position", &sf::Listener::setPosition,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Set the position of the listener in the scene
+
+	 //      The default listener's position is (0, 0, 0).
+
+	 //      \param position New listener's position
+
+	 //      \see getPosition, setDirection**)")
+
+	 // ,def("listener/get-position", &sf::Listener::getPosition,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Get the current position of the listener in the scene
+
+	 //      \return Listener's position
+
+	 //      \see setPosition**)")
+
+	 // ,def("listener/set-direction",
+	 //      (void (sf::Listener::*)(float float float))
+	 //      &sf::Listener::setDirection,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Set the forward vector of the listener in the scene
+
+	 //      The direction (also called "at vector") is the vector
+	 //      pointing forward from the listener's perspective. Together
+	 //      with the up vector, it defines the 3D orientation of the
+	 //      listener in the scene. The direction vector doesn't
+	 //      have to be normalized.
+	 //      The default listener's direction is (0, 0, -1).
+
+	 //      \param x X coordinate of the listener's direction
+	 //      \param y Y coordinate of the listener's direction
+	 //      \param z Z coordinate of the listener's direction
+
+	 //      \see getDirection, setUpVector, setPosition**)")
+
+	 // ,def("listener/set-direction",
+	 //      (void (sf::Listener::*)(const Vector3f &))
+	 //      &sf::Listener::setDirection,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Set the forward vector of the listener in the scene
+
+	 //      The direction (also called "at vector") is the vector
+	 //      pointing forward from the listener's perspective. Together
+	 //      with the up vector, it defines the 3D orientation of the
+	 //      listener in the scene. The direction vector doesn't
+	 //      have to be normalized.
+	 //      The default listener's direction is (0, 0, -1).
+
+	 //      \param direction New listener's direction
+
+	 //      \see getDirection, setUpVector, setPosition**)")
+
+	 // ,def("listener/get-direction", &sf::Listener::getDirection,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Get the current forward vector of the listener in the scene
+
+	 //      \return Listener's forward vector (not normalized)
+
+	 //      \see setDirection**)")
+
+	 // ,def("listener/set-up-vector",
+	 //      (float float float)
+	 //      &sf::Listener::setUpVector,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Set the upward vector of the listener in the scene
+
+	 //      The up vector is the vector that points upward from the
+	 //      listener's perspective. Together with the direction, it
+	 //      defines the 3D orientation of the listener in the scene.
+	 //      The up vector doesn't have to be normalized.
+	 //      The default listener's up vector is (0, 1, 0). It is usually
+	 //      not necessary to change it, especially in 2D scenarios.
+
+	 //      \param x X coordinate of the listener's up vector
+	 //      \param y Y coordinate of the listener's up vector
+	 //      \param z Z coordinate of the listener's up vector
+
+	 //      \see getUpVector, setDirection, setPosition**)")
+
+	 // ,def("listener/set-up-vector",
+	 //      (void (sf::Listener::*)(const Vector3f &))
+	 //      &sf::Listener::setUpVector,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Set the upward vector of the listener in the scene
+
+	 //      The up vector is the vector that points upward from the
+	 //      listener's perspective. Together with the direction, it
+	 //      defines the 3D orientation of the listener in the scene.
+	 //      The up vector doesn't have to be normalized.
+	 //      The default listener's up vector is (0, 1, 0). It is usually
+	 //      not necessary to change it, especially in 2D scenarios.
+
+	 //      \param upVector New listener's up vector
+
+	 //      \see getUpVector, setDirection, setPosition**)")
+
+	 // ,def("listener/get-up-vector", &sf::Listener::getUpVector,
+	 //      policies<>(), "", "",
+	 //      R"**(\brief Get the current upward vector of the listener in the scene
+
+	 //      \return Listener's upward vector (not normalized)
+
+	 //      \see setUpVector**)")
+	 
 	 class_<sf::SoundBuffer>("sound-buffer")
 	 .def_constructor("make-sound-buffer", constructor<const sf::SoundBuffer&>())
 	 .def("load-from-file", &sf::SoundBuffer::loadFromFile,
@@ -195,30 +333,11 @@ void CLASP_MAIN()
 	      policies<>(), "", "",
 	      R"**(\brief Get the sample rate
 
-	      The sample rate defines the number of audio samples
-	      captured per second. The higher, the better the quality
-	      (for example, 44100 samples/sec is CD quality).
+The sample rate defines the number of audio samples
+captured per second. The higher, the better the quality
+(for example, 44100 samples/sec is CD quality).
 
-	      \return Sample rate, in samples per second)**")
-
-	 // .def("get-available-devices", &sf::SoundRecorder::getAvailableDevices,
-	 //      policies<>(), "", "",
-	 //      R"**(\brief Get a list of the names of all available audio capture devices
-
-	 //      This function returns a vector of strings, containing
-	 //      the names of all available audio capture devices.
-
-	 //      \return A vector of strings containing the names)**")
-
-	 // .def("get-default-device", &sf::SoundRecorder::getDefaultDevice,
-	 //      policies<>(), "", "",
-	 //      R"**(\brief Get the name of the default audio capture device
-
-	 //      This function returns the name of the default audio
-	 //      capture device. If none is available, an empty string
-	 //      is returned.
-
-	 //      \return The name of the default audio capture device)**")
+\return Sample rate, in samples per second)**")
 
 	 .def("set-device", &sf::SoundRecorder::setDevice,
 	      policies<>(), "", "",
@@ -240,17 +359,36 @@ void CLASP_MAIN()
 	      R"**(\brief Get the name of the current audio capture device
 
 	      \return The name of the current audio capture device)**")
+	 
+	 ,def("sound-recorder/get-available-devices", &sf::SoundRecorder::getAvailableDevices,
+	      policies<>(), "", "",
+	      R"**(\brief Get a list of the names of all available audio capture devices
 
-	 // .def("is-available", &sf::SoundRecorder::isAvailable,
-	 //      policies<>(), "", "",
-	 //      R"**(\brief Check if the system supports audio capture
+	      This function returns a vector of strings, containing
+	      the names of all available audio capture devices.
 
-	 //      This function should always be called before using
-	 //      the audio capture features. If it returns false, then
-	 //      any attempt to use sf::SoundRecorder or one of its derived
-	 //      classes will fail.
+	      \return A vector of strings containing the names)**")
 
-	 //      \return True if audio capture is supported, false otherwise)**")
+	 ,def("sound-recorder/get-default-device", &sf::SoundRecorder::getDefaultDevice,
+	      policies<>(), "", "",
+	      R"**(\brief Get the name of the default audio capture device
+
+	      This function returns the name of the default audio
+	      capture device. If none is available, an empty string
+	      is returned.
+
+	      \return The name of the default audio capture device)**")
+
+	 ,def("sound-recorder/is-available", &sf::SoundRecorder::isAvailable,
+	      policies<>(), "", "",
+	      R"**(\brief Check if the system supports audio capture
+
+	      This function should always be called before using
+	      the audio capture features. If it returns false, then
+	      any attempt to use sf::SoundRecorder or one of its derived
+	      classes will fail.
+
+	      \return True if audio capture is supported, false otherwise)**")
 
 	 ,
 	 class_<sf::SoundSource>("sound-source", no_default_constructor)
